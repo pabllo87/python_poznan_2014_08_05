f0b648c5befc04f773bb901e2fc7b71cd69e98e0
Jacek Pospychala <jacek.pospychala@currency-one.com>
2014-08-07 14:35:49 +0200

    move unconditionals to top where possible

--- gilded_rose/gilded_rose_original.py
+++ gilded_rose/gilded_rose_original.py
@@ -20,18 +20,18 @@ class GildedRose(object):
                 if item.sell_in < 0:
                     item.quality = item.quality - item.quality
             elif item.name == "Aged Brie":
+                item.sell_in = item.sell_in - 1
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    if item.quality < 50:
+                if item.quality < 50:
+                    if item.sell_in < 0:
                         item.quality = item.quality + 1
             elif item.name == "Sulfuras, Hand of Ragnaros":
                 pass
             else: # regular
+                item.sell_in = item.sell_in - 1
                 if item.quality > 0:
                     item.quality = item.quality - 1
-                item.sell_in = item.sell_in - 1
                 if item.sell_in < 0:
                     if item.quality > 0:
                         item.quality = item.quality - 1
