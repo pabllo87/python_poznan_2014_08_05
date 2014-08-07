822f80fc379b792d36fa8a59ecfbdf31344b61de
Jacek Pospychala <jacek.pospychala@currency-one.com>
2014-08-07 14:29:28 +0200

    refactor second if

--- gilded_rose/gilded_rose_original.py
+++ gilded_rose/gilded_rose_original.py
@@ -27,17 +27,20 @@ class GildedRose(object):
                 if item.quality > 0:
                     item.quality = item.quality - 1
                 item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+            
+            if item.name == "Aged Brie":
+                if item.sell_in < 0:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 0:
+                    item.quality = item.quality - item.quality
+            else:
+                if item.sell_in < 0:
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            item.quality = item.quality - 1
+                        
 
     
 class Item:
