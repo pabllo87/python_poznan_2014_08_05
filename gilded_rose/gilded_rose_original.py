7848187e0769726347b877850aff273fdf1c326f
Jacek Pospychala <jacek.pospychala@currency-one.com>
2014-08-07 14:31:31 +0200

    merge two ifs

--- gilded_rose/gilded_rose_original.py
+++ gilded_rose/gilded_rose_original.py
@@ -32,18 +32,9 @@ class GildedRose(object):
                 if item.quality > 0:
                     item.quality = item.quality - 1
                 item.sell_in = item.sell_in - 1
-            
-            if item.name == "Aged Brie":
-                pass
-            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
-                pass
-            elif item.name == "Sulfuras, Hand of Ragnaros":
-                pass
-            else:
                 if item.sell_in < 0:
                     if item.quality > 0:
                         item.quality = item.quality - 1
-                        
 
     
 class Item:
